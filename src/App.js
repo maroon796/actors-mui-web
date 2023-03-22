3798953abc816d3365934c95a6eefd7f2602dd3d
Grygory Ksheminsky <maroon785@gmail.com>
2023-03-22 21:04:29 +0200

    working on theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import './App.css';
+import { Button, styled, Typography } from '@mui/material';
+import { Settings, Add } from '@mui/icons-material';
 
 function App() {
-  return <div>App</div>;
+  const BlueButton = styled(Button)(({ theme }) => ({
+    color: 'green',
+    backgroundColor: theme.palette.secondary.light,
+    margin: 5,
+    '&:hover': {
+      backgroundColor: 'black',
+    },
+    '&:disabled': {
+      backgroundColor: 'goldenrod',
+    },
+  }));
+
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained" color="secondary" endIcon={<Settings />}>
+        Settings
+      </Button>
+      <Button variant="contained" color="otherColor" startIcon={<Add />}>
+        Add New Post
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        It uses h1 style but it's a p tag
+      </Typography>
+      <BlueButton>BUTTON</BlueButton>
+      <BlueButton>ANOTHER BUTTON</BlueButton>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#7FBCD2',
+    },
+    secondary: {
+      main: '#A5F1E9',
+      light: '#E1FFEE',
+    },
+    otherColor: {
+      main: '#FFEEAF',
+    },
+  },
+});

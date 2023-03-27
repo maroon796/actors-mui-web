c5217d94f0ec997692068adf7a4a03dce9ac0c11
Grygory Ksheminsky <maroon785@gmail.com>
2023-03-27 16:19:31 +0300

    another test commit

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -4,7 +4,6 @@ const Rightbar = () => {
   return (
     <Box bgcolor="#E1FFEE" flex={2} p={2} sx={{ display: { xs: 'none', md: 'block' } }}>
       <Box position="fixed">s</Box>
-      <Box position="fixed">s</Box>
     </Box>
   );
 };

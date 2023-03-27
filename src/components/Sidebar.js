cbd061f0f9d3fc04592a2d184fece69faeb51ea8
Grygory Ksheminsky <maroon785@gmail.com>
2023-03-27 15:08:47 +0300

    working with Sidebar and dark mode switch button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,83 @@
-import { Box } from '@mui/material';
+import {
+  AccountBox,
+  AppSettingsAlt,
+  Home,
+  Info,
+  LocalGroceryStore,
+  ModeNight,
+  SettingsAccessibility,
+} from '@mui/icons-material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from '@mui/material';
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="royalblue" flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#settings">
+            <ListItemIcon>
+              <AppSettingsAlt />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#market">
+            <ListItemIcon>
+              <LocalGroceryStore />
+            </ListItemIcon>
+            <ListItemText primary="Market" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#profile">
+            <ListItemIcon>
+              <SettingsAccessibility />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#about">
+            <ListItemIcon>
+              <Info />
+            </ListItemIcon>
+            <ListItemText primary="About" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#friends">
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch defaultChecked color="secondary" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
